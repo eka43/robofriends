dcbbfe41e7d5068f78cd9172ef2d26df6f07d86a
Eunji Kang <eka43@sfu.ca>
2020-05-05 21:39:54 -0700

    Added files

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -25,23 +24,22 @@ class App extends Component {
 	}
 
 	render() {
-		const { robots, searchfield } = this.state;
-		const filteredRobots = robots.filter(robot =>{
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return !robots.length?
-		<h1>Loading</h1> :
-		(
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<Scroll>
-					<ErrorBoundry>
+		if (this.state.robots.length === 0) {
+			return <h1>Loading</h1>
+		} else {
+			return (
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
 						<CardList robots={filteredRobots}/>
-					</ErrorBoundry>
-				</Scroll>
-			</div>
-		);			
+					</Scroll>
+				</div>
+			);			
+		}
 	}
 }
 
